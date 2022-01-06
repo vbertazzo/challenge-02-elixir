4477214dc4a20cb4caff4818e444c346180ffb48
vbertazzo <vbertazzo@gmail.com>
2022-01-06 19:14:04 -0300

    Add ListFilter call function

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -4,15 +4,26 @@ defmodule ListFilter do
   """
 
   @doc """
-  Hello world.
+  Returns odd number's count from string list.
 
   ## Examples
 
-      iex> ListFilter.hello()
-      :world
+      iex> ListFilter.call(["6", "42", "banana", "6", "abc"])
+      0
+
+      iex> ListFilter.call(["1", "3", "6", "43", "banana", "6", "abc"])
+      3
 
   """
-  def hello do
-    :world
+  def call(list) do
+    Enum.flat_map(list, fn string ->
+      case Integer.parse(string) do
+        {int, _rest} -> [int]
+        :error -> []
+      end
+    end)
+    |> Enum.count(fn x ->
+      rem(x, 2) != 0
+    end)
   end
 end
--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -2,7 +2,30 @@ defmodule ListFilterTest do
   use ExUnit.Case
   doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  test "empty list" do
+    list = []
+
+    result = ListFilter.call(list)
+    expected = 0
+
+    assert result == expected
+  end
+
+  test "list with only even numbers" do
+    list = ["12", "4", "6", "10", "6"]
+
+    result = ListFilter.call(list)
+    expected = 0
+
+    assert result == expected
+  end
+
+  test "list with strings and odd/even numbers" do
+    list = ["1", "3", "6", "43", "banana", "6", "abc"]
+
+    result = ListFilter.call(list)
+    expected = 3
+
+    assert result == expected
   end
 end
